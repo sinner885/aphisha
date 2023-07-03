96bcaabc3c06bf6f8f57f28a6869174c07ebfffd
sinner <sinner885@gmail.com>
2023-07-03 18:21:22 +0300

    smtp настройки

--- config/settings.py
+++ config/settings.py
@@ -181,7 +181,7 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
 )
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False 
 ACCOUNT_USERNAME_REQUIRED = False
@@ -196,3 +196,12 @@ ACCOUNT_FORMS = {
 }
 
 DEFAULT_FROM_EMAIL = "admin@infopogreb.com"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ingopogreb@gmail.com'
+EMAIL_HOST_PASSWORD = 'ensjijkduzsbdcsr'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
