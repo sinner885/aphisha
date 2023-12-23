6a626fa54e73a086e109545ada0bf375c80bc9bd
sinner <sinner885@gmail.com>
2023-12-23 16:35:35 +0200

    pagination

--- adverts/views.py
+++ adverts/views.py
@@ -8,13 +8,18 @@ class AdvertPageView(ListView):
     """Вывод на странице Объявления"""
     model = Advert
     template_name = "adverts/adverts.html"
-    #context_object_name = 'adverts'
+    context_object_name = 'adverts'
+    paginate_by = 3
 
+    def get_queryset(self):
+        queryset = Advert.custom.moderation().order_by('-created')
+        return queryset
+        
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Оголошення'
         context['categorys'] = Category.objects.all()
-        context['adverts'] = Advert.custom.moderation().order_by('-created')
+        #context['adverts'] = Advert.custom.moderation().order_by('-created')
         return context
 
 
@@ -27,6 +32,7 @@ class AdvertDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = self.object.subject
+        context['categorys'] = Category.objects.all()
         return context
 
 
@@ -36,11 +42,12 @@ class AdvertByCategoryListView(ListView):
     model = Advert
     template_name = 'adverts/adverts.html'
     context_object_name = 'adverts'
+    
     category = None
 
     def get_queryset(self):
         self.category = Category.objects.get(slug=self.kwargs['slug'])
-        queryset = Advert.objects.all().filter(category__slug=self.category.slug)
+        queryset = Advert.custom.moderation().order_by('-created').filter(category__slug=self.category.slug)
         return queryset
        
 
