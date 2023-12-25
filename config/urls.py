3310200dea9d15416984e67f628efbc5bea2e124
sinner <sinner885@gmail.com>
2023-12-25 13:05:06 +0200

    paginator++

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,7 @@ urlpatterns = [
     path("", include("pages.urls")),
     path("", include("adverts.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns = [path('__debug__/', include('debug_toolbar.urls'))] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
